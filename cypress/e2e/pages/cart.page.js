a1e0ac0f30e6cf61dff1e4b2348379aabcabbc5c
NicMait <nicolemtlnd@gmail.com>
2022-12-12 20:57:22 -0500

    commit

--- cypress/e2e/pages/cart.page.js
+++ cypress/e2e/pages/cart.page.js
@@ -1,7 +1,15 @@
 class Cart{
 
 	get cartBtn () { return ('#top-cart')}
-	
+	get deleteProduct() { return ('[title="Remove item"]')}
+
+
+	cartDetails(){
+		cy.get(this.cartBtn).scrollIntoView()
+				cy.wait(1500)
+		cy.get(this.cartBtn).click()
+
+		}
 }
 
 
--- cypress/e2e/tests/cart.cy.js
+++ cypress/e2e/tests/cart.cy.js
@@ -2,12 +2,12 @@ import Authentication from '../pages/authentication.page'
 import AddToCart from '../pages/addtocart.page'
 import Cart from '../pages/cart.page'
 
-describe('Authentication', () => {
+describe('Cart', () => {
 	beforeEach(() => {
 			cy.visit('/')
 		})
 
-	it('Delete a product from cart', () => {
+	it('View products in cart', () => {
 
 			// Login with valid credentials
 			cy.get('#signInOrRegister').should('be.visible')
@@ -16,11 +16,39 @@ describe('Authentication', () => {
 
 			//Add products to the cart
 			AddToCart.addToCart()
-			AddToCart.addToCartTwo()
+	    cy.get(AddToCart.backToShopping).should('be.visible')
+    	cy.get (AddToCart.backToShopping).click()
+	    AddToCart.addToCartTwo()
+	    cy.get(AddToCart.cartSummary).should('be.visible')
+      cy.get(AddToCart.cartSummary).should('have.text',' Cart summary ')
+			cy.get(AddToCart.backToShopping).should('be.visible')
+    	cy.get (AddToCart.backToShopping).click()
 
 			//Navigate to cart
-			cy.get(Cart.cartBtn).should('be.visible')
-			cy.get(Cart.cartBtn).click()
+      Cart.cartDetails()
+	})
+
+
+	it.only('Delete a product from cart', () => {
+
+		// Login with valid credentials
+		cy.get('#signInOrRegister').should('be.visible')
+		cy.get('#signInOrRegister').click()
+		Authentication.login('marsenal2@yopmail.com', 'Password123!')
+
+		//Add a product to the cart
+		AddToCart.addToCart()
+		cy.get(AddToCart.backToShopping).should('be.visible')
+		cy.get (AddToCart.backToShopping).click()
+
+		//Navigate to cart
+		Cart.cartDetails()
+
+	// Remove a product from the cart
+    cy.get(Cart.deleteProduct).click()
+
+
+
 
 	})
 
